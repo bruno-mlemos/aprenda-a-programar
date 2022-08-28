b71873dee91cd6caebc72a6e78dfd09f3bf90861
Bruno Lemos <brunomlemos.ti@gmail.com>
2022-08-27 21:28:40 -0300

    atualização geral de pastas

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,12 @@
+//TABUADA
+
+let n = 9;
+let contador = 0;
+let multiplicador = 1;
+
+console.log("TABUADA DE 9");
+
+while (contador <= 9){
+    multiplicador = contador*n;
+    console.log(`${n} x ${contador} = ${multiplicador}`);
+}
